9877b44d8963f27114346a736cd5f4afe48556e0
Ke Qu <quke@microsoft.com>
2023-12-08 11:26:49 +0800

    update virtual main.tf to be consistent with physical

--- dev/sample/main.tf
+++ dev/sample/main.tf
@@ -1,62 +1,57 @@
-# variable "subscriptionId" {
-#   description = "The subscription id to register this environment."
-#   type        = string
-#   default     = "<yourSub>"
-# }
-
-# locals {
+locals {
 #   siteId = "<yourSiteID>"
-# }
+}
 
-# module "base" {
-#   source          = "../../modules/hci"
-#   location        = "eastus"
-#   siteId          = local.siteId
-#   domainFqdn      = "jumpstart.local"
-#   startingAddress = "192.168.1.15"
-#   endingAddress   = "192.168.1.115"
-#   defaultGateway  = "192.168.1.1"
-#   dnsServers      = ["192.168.1.254"]
-#   adouPath        = "OU=${local.siteId},DC=jumpstart,DC=local"
-#   domainServerIP  = "192.168.1.1"
-#   servers = [
-#     {
-#       name        = "AzSHOST1",
-#       ipv4Address = "192.168.1.12"
-#     },
-#     {
-#       name        = "AzSHOST2",
-#       ipv4Address = "192.168.1.13"
-#     }
-#   ]
-#   managementAdapters = ["FABRIC", "FABRIC2"]
-#   storageNetworks = [
-#     {
-#       name               = "Storage1Network",
-#       networkAdapterName = "StorageA",
-#       vlanId             = "711"
-#     },
-#     {
-#       name               = "Storage2Network",
-#       networkAdapterName = "StorageB",
-#       vlanId             = "712"
-#     }
-#   ]
-#   subId = var.subscriptionId
-#   // Beginning of specific varible for virtual environment
-#   dcPort = 6985
-#   serverPorts = {
-#     "AzSHOST1" = 15985,
-#     "AzSHOST2" = 25985
-#   }
+module "base" {
+  source          = "../../modules/hci"
+  location        = "eastus"
+  siteId          = local.siteId
+  domainFqdn      = "jumpstart.local"
+  startingAddress = "192.168.1.15"
+  endingAddress   = "192.168.1.115"
+  defaultGateway  = "192.168.1.1"
+  dnsServers      = ["192.168.1.254"]
+  adouPath        = "OU=${local.siteId},DC=jumpstart,DC=local"
+  domainServerIP  = "192.168.1.1"
+  servers = [
+    {
+      name        = "AzSHOST1",
+      ipv4Address = "192.168.1.12"
+    },
+    {
+      name        = "AzSHOST2",
+      ipv4Address = "192.168.1.13"
+    }
+  ]
+  managementAdapters = ["FABRIC", "FABRIC2"]
+  storageNetworks = [
+    {
+      name               = "Storage1Network",
+      networkAdapterName = "StorageA",
+      vlanId             = "711"
+    },
+    {
+      name               = "Storage2Network",
+      networkAdapterName = "StorageB",
+      vlanId             = "712"
+    }
+  ]
+  // Beginning of specific varible for virtual environment
+  dcPort = 6985
+  serverPorts = {
+    "AzSHOST1" = 15985,
+    "AzSHOST2" = 25985
+  }
 #   virtualHostIp = "<yourVirutalIP>"
-#   // end of specific varible for virtual environment
-#   tenant                 = "<yourTenant>"
-#   domainAdminUser        = "<domainAdminUser>"
-#   domainAdminPassword    = "<domainAdminPassword>"
-#   localAdminUser         = "<localAdminUser>"
-#   localAdminPassword     = "<localAdminPassword>"
-#   servicePrincipalId     = "<servicePrincipalId>"
-#   servicePrincipalSecret = "<servicePrincipalSecret>"
+  // end of specific varible for virtual environment
+  tenant = var.tenant
+  subId = var.subscriptionId
 
-# }
+  domainAdminUser        = var.domainAdminUser
+  domainAdminPassword    = var.domainAdminPassword
+  localAdminUser         = var.localAdminUser
+  localAdminPassword     = var.localAdminPassword
+  servicePrincipalId     = var.servicePrincipalId
+  servicePrincipalSecret = var.servicePrincipalSecret
+
+}
---
+++ dev/sample/variables.tf
@@ -0,0 +1,38 @@
+variable "localAdminUser" {
+  description = "The username of the local administrator account."
+  sensitive   = true
+  type        = string
+}
+variable "localAdminPassword" {
+  description = "The password of the local administrator account."
+  sensitive   = true
+  type        = string
+}
+variable "domainAdminUser" {
+  description = "The username of the domain account."
+  sensitive   = true
+  type        = string
+}
+variable "domainAdminPassword" {
+  description = "The password of the domain account."
+  sensitive   = true
+  type        = string
+}
+variable "servicePrincipalId" {
+  description = "The id of service principal to create hci cluster."
+  sensitive   = true
+  type        = string
+}
+variable "servicePrincipalSecret" {
+  description = "The secret of service principal to create hci cluster."
+  sensitive   = true
+  type        = string
+}
+variable "subscriptionId" {
+  description = "The subscription id to register this environment."
+  type        = string
+}
+variable "tenant" {
+  description = "The tenant id to register this environment."
+  type        = string
+}
