43cc047d7e858d360c35ff248eb0a1767f8e20b3
xwen11 <exporter@iac.microsoft.com>
2024-05-29 18:55:28 -0700

    creating 1 dev site and 1 prod site

--- dev/sample/main.tf
+++ dev/sample/main.tf
@@ -1,66 +1,74 @@
-# module "base" {
-#   source          = "../../modules/base"
-#   location        = "<location>"
-#   siteId          = basename(abspath(path.module))
-#   domainFqdn      = "<domainFqdn>"
-#   startingAddress = "<startingAddress>"
-#   endingAddress   = "<endingAddress>"
-#   defaultGateway  = "<defaultGateway>"
-#   dnsServers      = ["<dnsServer1>"]
-#   adouSuffix      = "<adouSuffix>"
-#   domainServerIP  = "<domainServerIP>"
-#   servers = [
-#     {
-#       name        = "<server1Name>",
-#       ipv4Address = "<server1Ipv4Address>"
-#     },
-#     {
-#       name        = "<server2Name>",
-#       ipv4Address = "<server2Ipv4Address>"
-#     }
-#   ]
-#   managementAdapters = ["<managementAdapter1>", "<managementAdapter2>"]
-#   storageNetworks = [
-#     {
-#       name               = "Storage1Network",
-#       networkAdapterName = "<storageAdapter1>",
-#       vlanId             = "<storageAdapter1Vlan>"
-#     },
-#     {
-#       name               = "Storage2Network",
-#       networkAdapterName = "<storageAdapter2>",
-#       vlanId             = "<storageAdapter2Vlan>"
-#     }
-#   ]
-#   rdmaEnabled                   = false     // Change to true if RDMA is enabled.
-#   storageConnectivitySwitchless = false     // Change to true if storage connectivity is switchless.
-#   enableProvisioners            = true      // Change to false when Arc servers are connected by yourself.
-#   authenticationMethod          = "Credssp" // or "Default"
-#   subscriptionId                = var.subscriptionId
-#   domainAdminUser               = var.domainAdminUser
-#   domainAdminPassword           = var.domainAdminPassword
-#   localAdminUser                = var.localAdminUser
-#   localAdminPassword            = var.localAdminPassword
-#   deploymentUserPassword        = var.deploymentUserPassword
-#   servicePrincipalId            = var.servicePrincipalId
-#   servicePrincipalSecret        = var.servicePrincipalSecret
-#   rpServicePrincipalObjectId    = var.rpServicePrincipalObjectId
+module "base" {
+  source          = "../../modules/base"
+  location        = "eastus"
+  siteId          = basename(abspath(path.module))
+  domainFqdn      = "jumpstart.local"
+  startingAddress = "192.168.1.55"
+  endingAddress   = "192.168.1.65"
+  defaultGateway  = "192.168.1.1"
+  dnsServers      = ["192.168.1.254"]
+  adouSuffix      = "DC=jumpstart,DC=local"
+  domainServerIP  = "192.168.1.254"
+  servers = [
+    {
+      name        = "AzSHOST1",
+      ipv4Address = "192.168.1.12"
+    },
+    {
+      name        = "AzSHOST2",
+      ipv4Address = "192.168.1.13"
+    }
+  ]
+  managementAdapters = ["FABRIC", "FABRIC2"]
+  storageNetworks = [
+    {
+      name               = "Storage1Network",
+      networkAdapterName = "StorageA",
+      vlanId             = "711"
+    },
+    {
+      name               = "Storage2Network",
+      networkAdapterName = "StorageB",
+      vlanId             = "712"
+    }
+  ]
+  rdmaEnabled                   = false     // Change to true if RDMA is enabled.
+  storageConnectivitySwitchless = false     // Change to true if storage connectivity is switchless.
+  enableProvisioners            = true      // Change to false when Arc servers are connected by yourself.
+  authenticationMethod          = "Credssp" // or "Default"
+  subscriptionId                = var.subscriptionId
+  domainAdminUser               = var.domainAdminUser
+  domainAdminPassword           = var.domainAdminPassword
+  localAdminUser                = var.localAdminUser
+  localAdminPassword            = var.localAdminPassword
+  deploymentUserPassword        = var.deploymentUserPassword
+  servicePrincipalId            = var.servicePrincipalId
+  servicePrincipalSecret        = var.servicePrincipalSecret
+  rpServicePrincipalObjectId    = var.rpServicePrincipalObjectId
 
 
-#   # Region Hybrid AKS related parameters
-#   aksArc-controlPlaneIp = "<aksArc-controlPlaneIp>"
+  # Region Hybrid AKS related parameters
+  aksArc-controlPlaneIp = "192.168.1.190"
 
-#   // the following value only need to provide if you want to create a new logical network, if not, set aksArc-lnet-usingExistingLogicalNetwork to true and specify the existing logical network name in logicalNetworkName
-#   aksArc-lnet-startingAddress = "<aksArc-lnet-startingAddress>"
-#   aksArc-lnet-endingAddress   = "<aksArc-lnet-endingAddress>"
-#   aksArc-lnet-addressPrefix   = "<aksArc-lnet-addressPrefix>"
-#   aksArc-lnet-defaultGateway  = "<aksArc-lnet-defaultGateway>"
-#   aksArc-lnet-dnsServers      = ["<aksArc-lnet-dnsServer>"]
-#   rbacAdminGroupObjectIds     = ["<rbacAdminGroupObjectId1>"]
-#   # End region of hybrid aks related parameters
+  // the following value only need to provide if you want to create a new logical network, if not, set aksArc-lnet-usingExistingLogicalNetwork to true and specify the existing logical network name in logicalNetworkName
+  aksArc-lnet-startingAddress = "192.168.1.171"
+  aksArc-lnet-endingAddress   = "192.168.1.190"
+  aksArc-lnet-addressPrefix   = "192.168.1.0/24"
+  aksArc-lnet-defaultGateway  = "192.168.1.1"
+  aksArc-lnet-dnsServers      = ["192.168.1.254"]
+  rbacAdminGroupObjectIds     = ["1b81c027-029d-434d-9642-376e392bfe65"]
+  # End region of hybrid aks related parameters
 
-#   # Region site manager parameters
-#   # Check ./doc/Add-Site-Manager.md for more information
-#   country = "<country>"
-#   # End region site manager parameters
-# }
+  # Region site manager parameters
+  # Check ./doc/Add-Site-Manager.md for more information
+  country         = "US"
+  city            = "redmond"
+  companyName     = "contoso"
+  postalCode      = "4331"
+  stateOrProvince = "unkown"
+  streetAddress1  = "1 microsoft way"
+  contactName     = "qfai"
+  emailList       = ["xxx@contoso.com"]
+  mobile          = "xxxx"
+  phone           = "xxxx"
+ }
